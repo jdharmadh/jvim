f41e016ebcbdddd373c1a88fcbb801d78ecf9f2c
Jay Dharmadhikari <jayd1903@gmail.com>
2024-11-16 23:24:07 -0800

    git test

--- jvim.c
+++ jvim.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+
+struct termios orig_termios;
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+  setvbuf(stdout, NULL, _IONBF, 0);
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO | ICANON);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 
 int main(int argc, char **argv){
+    enableRawMode();
     printf("\e[1;1H\e[2J");
-    printf("Hello World!\n");
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+        printf("%c", c);
+    }
+
     return 0;
 }
